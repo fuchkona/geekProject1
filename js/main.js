7b749111dce4cf4751383271cc442173f5c3b127
Fuchko Nikita <fuchko.nikita@gmail.com>
2017-12-23 21:04:01 +0500

    update deprecated ready function

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
-$('#browse-btn').on('click', function () {
-    toogle_browse_show();
-});
+$(function () {
+    $('#browse-btn').on('click', function () {
+        toogle_browse_show();
+    });
 
-$('#browse-categories').on('mouseleave', function () {
-    toogle_browse_show();
-});
+    $('#browse-categories').on('mouseleave', function () {
+        toogle_browse_show();
+    });
 
-function toogle_browse_show() {
-    var $body = $('#browse-categories');
-    var $btn = $('#browse-btn');
-    $body.css({top: $btn.offset().top + $btn.height() + 4, left: $btn.offset().left - 8});
-    $body.toggleClass('show');
-}
+    function toogle_browse_show() {
+        var $body = $('#browse-categories');
+        var $btn = $('#browse-btn');
+        $body.css({top: $btn.offset().top + $btn.height() + 4, left: $btn.offset().left - 8});
+        $body.toggleClass('show');
+    }
 
-function goToUrl(url) {
-    window.location = url;
-}
+    function goToUrl(url) {
+        window.location = url;
+    }
 
-$('.products-item-hover, .featured-items-item-hover').on('click', function (e) {
-    if (e.target !== this)
-        return;
-    goToUrl('product.html')
+    $('.products-item-hover, .featured-items-item-hover').on('click', function (e) {
+        if (e.target !== this)
+            return;
+        goToUrl('product.html')
+    });
 });
